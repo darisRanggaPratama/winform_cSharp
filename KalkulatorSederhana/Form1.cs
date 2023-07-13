e5c79e1a6536ef8a25f460d80e0800ae0b9c9027
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-13 23:02:41 +0700

    initial commit

---
+++ KalkulatorSederhana/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KalkulatorSederhana
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void btnTambah_Click(object sender, EventArgs e)
+		{
+			int angka1 = Int32.Parse(txt1.Text);
+			int angka2 = Int32.Parse(txt2.Text);
+			int hasil = angka1 + angka2;
+
+			txt3.Text = hasil.ToString();
+		}
+
+		private void btnKurang_Click(object sender, EventArgs e)
+		{
+			int angka1 = Int32.Parse(txt1.Text);
+			int angka2 = Int32.Parse(txt2.Text);
+			int hasil = angka1 - angka2;
+
+			txt3.Text = hasil.ToString();
+		}
+
+		private void btnKali_Click(object sender, EventArgs e)
+		{
+			int angka1 = Int32.Parse(txt1.Text);
+			int angka2 = Int32.Parse(txt2.Text);
+			int hasil = angka1 * angka2;
+
+			txt3.Text = hasil.ToString();
+		}
+
+		private void btnBagi_Click(object sender, EventArgs e)
+		{
+			int angka1 = Int32.Parse(txt1.Text);
+			int angka2 = Int32.Parse(txt2.Text);
+			int hasil = angka1 / angka2;
+
+			txt3.Text = hasil.ToString();
+		}
+
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			txt1.Text = "";
+			txt2.Text = "";
+			txt3.Text = "";
+
+		}
+
+		
+	}
+}
